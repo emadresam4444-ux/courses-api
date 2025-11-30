46ba05751fbe4ee841eb93e5a761895e8419e505
Emad-Eldeen <emadresam4444@gmail.com>
2025-11-30 04:02:12 +0200

    Add pagination and standardized error handling to course retrieval endpoints

--- Controllers/Course.js
+++ Controllers/Course.js
@@ -1,25 +1,27 @@
 const courseModel = require("../Models/Course");
 const asyncWrapper = require("../middleware/asyncWrapper");
+const AppError = require("../utils/customError");
 const httpStatusText = require("../utils/httpStatusText");
-const getCourses = asyncWrapper(async (req, res) => {
-  const courses = await courseModel.find(
-    {},
-    { __v: false, createdAt: 0, updatedAt: 0 }
-  );
+const getCourses = asyncWrapper(async (req, res, next) => {
+  const limit = req.query.limit;
+  const page = req.query.page;
+  const skip = (page - 1) * limit;
+  const courses = await courseModel
+    .find({}, { __v: false, createdAt: 0, updatedAt: 0 })
+    .limit(limit)
+    .skip(skip);
   if (courses.length === 0) {
-    return res
-      .status(200)
-      .json({ status: httpStatusText.FAIL, message: "No courses found" });
+    return next(new AppError("Courses is Not Found", 404, httpStatusText.FAIL));
   }
-  res.status(200).json({ status: httpStatusText.SUCCESS, data: courses });
+  res.status(200).json({ status: httpStatusText.SUCCESS, data: { courses } });
 });
-const getCourse = asyncWrapper(async (req, res) => {
+const getCourse = asyncWrapper(async (req, res, next) => {
   const courseId = req.params.courseId;
   const course = await courseModel.findById(courseId);
   if (!course) {
-    return res.status(200).json({ status: httpStatusText.FAIL, data: course });
+    return next(new AppError("Course is Not Exist", 404, httpStatusText.FAIL));
   }
-  res.status(200).json({ status: httpStatusText.SUCCESS, data: course });
+  res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
 });
 const addCourse = asyncWrapper(async (req, res) => {
   const { title, price, instructor, description, isPublished } = req.body;
